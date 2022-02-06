346a78f481136c284d5ac1a8f65638ab48294687
Gospodi <gospodijazzet@gmail.com>
2022-02-06 22:13:35 +0300

    Добавлены привязки прогресс баров и label

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+        
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
 }
 
